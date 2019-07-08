c34d38b80ccbecdfebf312df307d77ef42f6d856
Atarius Armstrong <atariusarmstrong@gmail.com>
2019-07-08 11:21:53 -0500

    applied bootstrap grid to recipes

--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Container, Col, Row, Card } from 'react-bootstrap'
 
 
 class Recipe extends Component {
@@ -7,9 +8,23 @@ class Recipe extends Component {
             <div>
                 {this.props.recipe &&
                     <div>
-                        {this.props.recipe.recipes.map((recipe, i) => (
-                            <div key={i}>{recipe.title}</div>
-                        ))}
+                        <Container>
+                            <Row>
+
+                                {this.props.recipe.recipes.map((recipe, i) => (
+                                    <Col md={4}>
+                                        <Card key={i}>
+                                            <Card.Img src={recipe.image_url} alt={recipe.title}/>
+                                            <Card.ImgOverlay>
+                                                <Card.Title>{recipe.title}</Card.Title>
+                                            </Card.ImgOverlay>
+                                        </Card>
+
+                                    </Col>
+                                ))}
+
+                            </Row>
+                        </Container>
                     </div>
                 }
             </div>
